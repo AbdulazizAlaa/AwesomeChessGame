df74473ebaf14f99731298b64eeefd928b8986b2
abdulaziz <abdulazizalaa@ymail.com>
2014-12-24 01:20:48 +0200

    code-refactoring  and adding some imags

---
+++ js/square.js
@@ -0,0 +1,13 @@
+var square = function(color, pieceID, x, y, width, height){
+    var s = 
+        {
+            'color': color, 
+            'pieceID': pieceID, 
+            'x': x, 
+            'y': y, 
+            'width': width, 
+            'height': height
+        };
+    
+    return s;
+};
